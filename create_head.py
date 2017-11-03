a16d4a1f708f63861f97974656c061d42c2ad0fa
frahlg <fredrik@ahlgren.nu>
2017-11-03 15:46:56 +0100

    train_rpm

---
+++ create_head.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+
+project_path = os.path.realpath('')
+
+dict_ = {'ae1_frp':'AE1 FUEL RACK POSIT:1742:mm:Average:900',
+             'ae2_frp':'AE2 FUEL RACK POSIT:2742:mm:Average:900',
+             'ae3_frp':'AE3 FUEL RACK POSIT:3742:mm:Average:900',
+             'ae4_frp':'AE4 FUEL RACK POSIT:4742:mm:Average:900',
+
+             'ae1_cac_P':'AE1 CA OTL CAC PRESS:1649:Bar:Average:900',
+             'ae2_cac_P':'AE2 CA OTL CAC PRESS:2649:Bar:Average:900',
+             'ae3_cac_P':'AE3 CA OTL CAC PRESS:3649:Bar:Average:900',
+             'ae4_cac_P':'AE4 CA OTL CAC PRESS:4649:Bar:Average:900',
+
+             'ae1_cac_ca':'AE1 EXH CA OUTET 1:1543:  C:Average:900',
+             'ae2_cac_ca':'AE2 EXH CA OUTET 1:2543:  C:Average:900',
+             'ae3_cac_ca':'AE3 EXH CA OUTET 1:3543:  C:Average:900',
+             'ae4_cac_ca':'AE4 EXH CA OUTET 1:4543:  C:Average:900',
+
+             'ae1_exh':'AE1 EXH MEAN VALUE:1591:  C:Average:900',
+             'ae2_exh':'AE2 EXH MEAN VALUE:2591:  C:Average:900',
+             'ae3_exh':'AE3 EXH MEAN VALUE:3591:  C:Average:900',
+             'ae4_exh':'AE4 EXH MEAN VALUE:4591:  C:Average:900',
+
+             'ae1_fo_P':'AE1 FO INLET PRESS:1603:Bar:Average:900',
+             'ae2_fo_P':'AE2 FO INLET PRESS:2603:Bar:Average:900',
+             'ae3_fo_P':'AE3 FO INLET PRESS:3603:Bar:Average:900',
+             'ae4_fo_P':'AE4 FO INLET PRESS:4603:Bar:Average:900',
+
+             'ae1_rpm':'AE1 ENG SPEED:1745:RPM:Average:900',
+             'ae2_rpm':'AE2 ENG SPEED:2745:RPM:Average:900',
+             'ae3_rpm':'AE3 ENG SPEED:3745:RPM:Average:900',
+             'ae4_rpm':'AE4 ENG SPEED:4745:RPM:Average:900',
+
+             'me1_frp':'ME1 FUEL RACK POSIT:10005:%:Average:900',
+             'me2_frp':'ME2 FUEL RACK POSIT:20005:%:Average:900',
+             'me3_frp':'ME3 FUEL RACK POSIT:30005:%:Average:900',
+             'me4_frp':'ME4 FUEL RACK POSIT:40005:%:Average:900',
+
+             'me1_ca_T':'ME1 CA TEMP COOL OUT:1343:C:Average:900',
+             'me2_ca_T':'ME2 CA TEMP COOL OUT:2343:C:Average:900',
+             'me3_ca_T':'ME3 CA TEMP COOL OUT:3343:C:Average:900',
+             'me4_ca_T':'ME4 CA TEMP COOL OUT:4343:C:Average:900',
+
+             'me1_cac_T':'ME1 CHARGE AIR TEMP:1347:C:Average:900',
+             'me2_cac_T':'ME2 CHARGE AIR TEMP:2347:C:Average:900',
+             'me3_cac_T':'ME3 CHARGE AIR TEMP:3347:C:Average:900',
+             'me4_cac_T':'ME4 CHARGE AIR TEMP:4347:C:Average:900',
+
+             'me1_exh_T':'ME1 EXH GAS MEAN:1125:C:Average:900',
+             'me2_exh_T':'ME2 EXH GAS MEAN:2125:C:Average:900',
+             'me3_exh_T':'ME3 EXH GAS MEAN:3125:C:Average:900',
+             'me4_exh_T':'ME4 EXH GAS MEAN:4125:C:Average:900',
+
+             'me1_rpm':'ME1 ENGINE SPEED:1364:rpm:Average:900',
+             'me2_rpm':'ME2 ENGINE SPEED:2364:rpm:Average:900',
+             'me3_rpm':'ME3 ENGINE SPEED:3364:rpm:Average:900',
+             'me4_rpm':'ME4 ENGINE SPEED:4364:rpm:Average:900',
+
+             'fo_booster_13':'FO BOOST 1 CONSUMPT:6165:m3/h:Average:900',
+             'fo_booster_24':'FO BOOST 2 CONSUMPT:6166:m3/h:Average:900'}
+#%%
+
+
+df = pd.DataFrame.from_dict(dict_,orient='index')
+df.to_excel('headers_dict.xlsx')
--- training_model.py
+++ training_model.py
@@ -17,134 +17,106 @@ print('Time to load database:', time.time()-t1)
 
 # Variable names.
 import var_names
-d = var_names.dict_
+d = var_names.d
+
 
 
 # Check if variables exist in the dictonary..
+# for names in d:
+#     if d[names] in list(df):
+#         pass
+#     else:
+#         print('*** VAR MISSING *** ', d[names], ' *** VAR MISSING ***')
 
-for names in d:
-    if d[names] in list(df):
-        #print(var_names[names])
-        pass
-    else:
-        print('*** VAR MISSING *** ', var_names[names], ' *** VAR MISSING ***')
-
-
+##
 # Features and target for Eng 1/3
 
-eng_13 = [d['ae1_frp'],
-          d['ae3_frp'],
-          d['ae1_cac_P'],
-          d['ae3_cac_P'],
-          d['ae1_cac_ca'],
-          d['ae3_cac_ca'],
-          d['ae1_exh'],
-          d['ae3_exh'],
-          d['ae1_fo_P'],
-          d['ae3_fo_P'],
-          d['ae1_rpm'],
+
+#%%
+
+gen = 50
+cores=-1 # use all of them
+
+
+test_name = str('gen_' + str(gen) + '_rpm_predictor_'+time.strftime('%y%m%d'))
+print('Training... test ...', test_name)
+
+eng_13 = [d['ae1_rpm'],
           d['ae3_rpm'],
-          d['me1_frp'],
-          d['me3_frp'],
-          d['me1_ca_T'],
-          d['me3_ca_T'],
-          d['me1_cac_T'],
-          d['me3_cac_T'],
-          d['me1_exh_T'],
-          d['me3_exh_T'],
           d['me1_rpm'],
           d['me3_rpm'],
           d['fo_booster_13']
           ]
 
-# Features and target for Eng 2/4
-
-eng_24 = [d['ae2_frp'],
-          d['ae4_frp'],
-          d['ae2_cac_P'],
-          d['ae4_cac_P'],
-          d['ae2_cac_ca'],
-          d['ae4_cac_ca'],
-          d['ae2_exh'],
-          d['ae4_exh'],
-          d['ae2_fo_P'],
-          d['ae4_fo_P'],
-          d['ae2_rpm'],
+eng_24 = [d['ae2_rpm'],
           d['ae4_rpm'],
-          d['me2_frp'],
-          d['me4_frp'],
-          d['me2_ca_T'],
-          d['me4_ca_T'],
-          d['me2_cac_T'],
-          d['me4_cac_T'],
-          d['me2_exh_T'],
-          d['me4_exh_T'],
           d['me2_rpm'],
           d['me4_rpm'],
           d['fo_booster_24']
           ]
 
-
-
 #%%
 
-# Feature selection
+####
+#### Training the first set with only rpm predictor
+####
 
-print('Which variables..')
-gen = int(float(input('How many generations?')))
-cores = int(float(input('How many cores? (-1 all cores')))
+print('Features and predictions for training 1:\n\nEngine 1_3:')
 
-#%%
+for n in eng_13:
+    print('- ',d[n])
+print('\nEngine 2_4:')
+for n in eng_24:
+    print('- ',d[n])
 
-# If we want an interactive list to choose variables from.
-#
-#
-# import inquirer
-# questions = [
-#     inquirer.Checkbox('Variables engine 1/3',
-#                     message='Choose variables for training',
-#                     choices=eng_13)
-#             ]
-# answ = inquirer.prompt(questions)
-# print(answ)
-
-
-#%%
-
-
-
-print('Features and predictions for training 1', labels_1_3, labels_2_4)
+print('\nDate: ',time.strftime('%y%m%d'))
+print('Time: ',time.strftime('%H:%M:%S'))
 #%%
 # Drop Nan from the DataFrame.
 
-df_1_3 = df[labels_1_3].dropna()
-df_2_4 = df[labels_2_4].dropna()
+df_1_3 = df[eng_13].dropna()
+df_2_4 = df[eng_24].dropna()
 
 # Create training arrays, X_13 is the features for engine pair 1 and 3
 
-X_13 = np.array(df_1_3.drop(labels='FO BOOST 1 CONSUMPT:6165:m3/h:Average:900',axis=1))
-y_13 = np.array(df_1_3['FO BOOST 1 CONSUMPT:6165:m3/h:Average:900'])
+X_13 = np.array(df_1_3.drop(labels='fo_booster_13',axis=1))
+y_13 = np.array(df_1_3['fo_booster_13'])
+
+
+print('Training with TPOT engine pair 1....', test_name)
+t1 = time.time()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75, test_size=0.25)
+tpot_13 = TPOTRegressor(generations=gen, population_size=50, verbosity=2, n_jobs=cores)
+tpot_13.fit(X_train, y_train.reshape(-1,))
+
+print(tpot_13.score(X_test,y_test))
+t2 = time.time()
+delta_time = t2-t1
+print('Time to train...:', delta_time)
+
+#%%
+
+print('Saving the model ...')
+tpot_13.export(test_name +,'.py')
+joblib.dump(tpot_13.fitted_pipeline_,test_name +,'.pk1')
+print(test_name, ' saved ... ')
+
+#%%
 
 # X_24 features for engine 2, 4
 
-X_24 = np.array(df_2_4.drop(labels='FO BOOST 2 CONSUMPT:6166:m3/h:Average:900',axis=1))
-y_24 = np.array(df_2_4['FO BOOST 2 CONSUMPT:6166:m3/h:Average:900'])
+X = np.array(df_2_4.drop(labels='fo_booster_24',axis=1))
+y = np.array(df_2_4['fo_booster_24'])
 
-
-#%%
-print('Training with TPOT engine pair 1....')
+print('Training with TPOT engine pair 2....', test_name)
 t1 = time.time()
 
-#  modeling with tpot
-# Number of generations, should be boosted if we want better results. 3 gens take ~ 30min on my macbook...
-gen = 50
-cores=-1 # use all of them
-
-X_train_13, X_test_13, y_train_13, y_test_13 = train_test_split(X_13, y_13, train_size=0.75, test_size=0.25)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75, test_size=0.25)
 tpot_13 = TPOTRegressor(generations=gen, population_size=50, verbosity=2, n_jobs=cores)
-tpot_13.fit(X_train_13, y_train_13.reshape(-1,))
+tpot_13.fit(X_train, y_train.reshape(-1,))
 
-print(tpot_13.score(X_test_13,y_test_13))
+print(tpot_13.score(X_test,y_test))
 t2 = time.time()
 delta_time = t2-t1
 print('Time to train...:', delta_time)
@@ -152,35 +124,6 @@ print('Time to train...:', delta_time)
 #%%
 
 print('Saving the model ...')
-
-tpot_13.export('train_tpot_13_gen50.py')
-joblib.dump(tpot_13.fitted_pipeline_,'train_tpot_13_gen50.pk1')
-
-print('Model saved ... ')
-
-
-#%%
-
-print('Training with TPOT engine pair 2....')
-t1 = time.time()
-
-#  modeling with tpot
-# Number of generations, should be boosted if we want better results. 3 gens take ~ 30min on my macbook...
-
-X_train_24, X_test_24, y_train_24, y_test_24 = train_test_split(X_24, y_24, train_size=0.75, test_size=0.25)
-tpot_24 = TPOTRegressor(generations=gen, population_size=50, verbosity=2, n_jobs=cores)
-tpot_24.fit(X_train_24, y_train_24.reshape(-1,))
-
-print(tpot_24.score(X_test_24,y_test_24))
-t2 = time.time()
-delta_time = t2-t1
-print('Time to train...:', delta_time)
-
-#%%
-
-print('Saving the model ...')
-
-tpot_24.export('train_tpot_24_gen50.py')
-joblib.dump(tpot_24.fitted_pipeline_,'train_tpot_24_gen50.pk1')
-
-print('Model saved ... ')
+tpot_13.export(test_name +,'.py')
+joblib.dump(tpot_13.fitted_pipeline_,test_name +,'.pk1')
+print(test_name, ' saved ... ')
