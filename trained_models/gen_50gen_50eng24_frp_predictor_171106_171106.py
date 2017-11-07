c68a22f8d3c3095c9932e9eefff19c675a49ba1f
Fredrik Ahlgren <fredrik@ahlgren.nu>
2017-11-07 08:00:40 +0100

    training_frp

---
+++ trained_models/gen_50gen_50eng24_frp_predictor_171106_171106.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from tpot.builtins import ZeroCount
+
+# NOTE: Make sure that the class is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1).values
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'].values, random_state=42)
+
+# Score on the training set was:-0.012156814002618088
+exported_pipeline = make_pipeline(
+    StandardScaler(),
+    ZeroCount(),
+    MinMaxScaler(),
+    ExtraTreesRegressor(bootstrap=False, max_features=0.6000000000000001, min_samples_leaf=1, min_samples_split=12, n_estimators=100)
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
